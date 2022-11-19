84802fca2afb13dcee18431b4759f71d99d44ea6
SiddharthaMadhav <gsmadhav653@gmail.com>
2022-11-19 12:12:28 +0530

    first commit

---
+++ nft-hardhat/scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
+  const deployedWhitelist = await whitelistContract.deploy(10);
+  await deployedWhitelist.deployed();
+  console.log("Whitelist contract address: ", deployedWhitelist.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
